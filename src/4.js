5afbfdf92e4ec157b1457ce826102611945c9f0c
neuzilviapiano456 <neuzilviapiano456@gmail.com>
2025-03-08 08:55:17 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,7 @@
+  <script>
+    function getRandomColor() {
+      var colors = ["blue", "red", "green", "yellow"];
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
+    console.log(getRandomColor());
+  </script>
