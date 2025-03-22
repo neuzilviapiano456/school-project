a27bcdfaa655b7236d043ea719ff654eea47cc72
neuzilviapiano456 <neuzilviapiano456@gmail.com>
2025-03-22 16:32:41 +0000

    Add 18.js

---
+++ src/18.js
@@ -0,0 +1,7 @@
+// Example of an if statement in JavaScript
+
+if (condition) {
+  // Code block to execute if the condition is true
+} else {
+  // Code block to execute if the condition is false
+}
